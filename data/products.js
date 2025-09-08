cd8c001bc52f0a58cffb5f3eec09a05e825fddcc
Nguyễn Hưng <50404761+nguyenhung-sg-pro@users.noreply.github.com>
2025-09-08 15:51:58 +0700

    Add files via upload
    init

---
+++ data/products.js
@@ -0,0 +1,60 @@
+// data/products.js – mock dataset (Lesson 9: Array & Object)
+export const PRODUCTS = [
+  {
+    id: 1,
+    name: "Áo thun FE Basic",
+    price: 149000,
+    stock: 12,
+    image: "https://picsum.photos/id/1011/800/600",
+    description: "Áo thun cotton 100%, thoáng mát, phù hợp mọi hoạt động."
+  },
+  {
+    id: 2,
+    name: "Bình giữ nhiệt Inox 500ml",
+    price: 199000,
+    stock: 8,
+    image: "https://picsum.photos/id/1060/800/600",
+    description: "Giữ nóng/lạnh đến 8 giờ, lớp vỏ sơn tĩnh điện chống trầy."
+  },
+  {
+    id: 3,
+    name: "Sổ tay chấm bi A5",
+    price: 89000,
+    stock: 20,
+    image: "https://picsum.photos/id/24/800/600",
+    description: "Giấy dày 100gsm, bìa dẻo, phù hợp ghi chép học tập."
+  },
+  {
+    id: 4,
+    name: "Tai nghe In-ear",
+    price: 299000,
+    stock: 5,
+    image: "https://picsum.photos/id/250/800/600",
+    description: "Âm trầm ấm, có micro đàm thoại, dây chống rối."
+  },
+   {
+    id: 5,
+    name: "Tai nghe In-ear",
+    price: 299000,
+    stock: 5,
+    image: "https://picsum.photos/id/560/800/600",
+    description: "Âm trầm ấm, có micro đàm thoại, dây chống rối."
+  },
+   {
+    id: 6,
+    name: "Tai nghe In-ear",
+    price: 299000,
+    stock: 5,
+    image: "https://picsum.photos/id/274/800/600",
+    description: "Âm trầm ấm, có micro đàm thoại, dây chống rối."
+  },
+   {
+    id: 7,
+    name: "Tai nghe In-ear",
+    price: 299000,
+    stock: 5,
+    image: "https://picsum.photos/id/354/800/600",
+    description: "Âm trầm ấm, có micro đàm thoại, dây chống rối."
+  }
+
+];
